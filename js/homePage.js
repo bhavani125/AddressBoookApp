50b159d705c6185242c33b69c142e60d3d5d64f7
bhavani125 <girinenibhavani12@gmil.com>
2021-09-04 00:08:39 +0530

    Added: Code of populating person list from local storage

--- js/homePage.js
+++ js/homePage.js
@@ -1,14 +1,21 @@
+let addressbookList;
 window.addEventListener('DOMContentLoaded', () => {
+    addressbookList = getPersonDataFromLocalStorage();
+    document.querySelector('.person-count').textContent = addressbookList.length;
     createInnerHTML();
 });
-
+//get the list of Person data from local storage
+const getPersonDataFromLocalStorage = () => {
+    return localStorage.getItem('AddressBookList') ?
+                        JSON.parse(localStorage.getItem('AddressBookList')) : [] ;
+}
 //Template literal ES6 feature 
 const createInnerHTML = () => {
+    if( addressbookList.length == 0 ) return;
     const headerHtml = "<th>Full Name</th><th>Address</th><th>City</th><th>State</th>"+
                        "<th>ZipCode</th><th>Phone Number</th><th>Actions</th>";
     let innerHtml = `${headerHtml}`;
-    let addressbookList = createAddressBookJSON();
-
+   
     for  ( const addressbookData of addressbookList) {
         innerHtml = `${innerHtml}
         <tr>
@@ -19,9 +26,9 @@ const createInnerHTML = () => {
             <td>${addressbookData._zipCode}</td>
             <td>${addressbookData._phoneNumber}</td>
             <td>
-            <img name="${addressbookData._id}" onclick="removie(this)" alt="delete" 
+            <img id="${addressbookData._id}" onclick="removie(this)" alt="delete" 
                     src="../assests/icons/delete-black-18dp.svg">
-            <img name="${addressbookData._id}" alt="edit" onclick="update(this)"
+            <img id="${addressbookData._id}" alt="edit" onclick="update(this)"
                     src="../assests/icons/create-black-18dp.svg">
             </td>
         </tr>
@@ -30,27 +37,3 @@ const createInnerHTML = () => {
     document.querySelector('#table-display').innerHTML=innerHtml;
 }
 
-//create Employee Payroll JSON objects
-const createAddressBookJSON = () => {
-    let addressbookList = [
-        {
-            _name : 'Bhavani',
-            _address : 'Nitture,Peddapallly',
-            _city : 'Hyderabad',
-            _state : 'Telangana',
-            _zipCode : '505174',
-            _phoneNumber : '+919989464121',
-            _id : new Date().getTime()
-        },
-        {
-            _name : 'Harish',
-            _address : 'Nitture,Peddapally',
-            _city : 'Hyderabad',
-            _state : 'Telangana',
-            _zipCode : '505174',
-            _phoneNumber : '+918341930630',
-            _id : new Date().getTime()
-        }
-    ];
-    return addressbookList;
-} 
